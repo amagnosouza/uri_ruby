40d3e5181ee3a819d6d1eaa6d6a4b0c4336118bc
Alexandre Souza <alexandremagnofs@gmail.com>
2019-10-15 10:54:20 -0300

    questoes 1016 a 1018

---
+++ src/uri_1016.rb
@@ -0,0 +1,3 @@
+d = gets.chomp.to_i
+#O problema diz que o carro y se afasta 1 km a cada 2 min, 
+puts "%d minutos" % [d * 2]
---
+++ src/uri_1017.rb
@@ -0,0 +1,4 @@
+temp = gets.chomp.to_i
+vm = gets.chomp.to_i
+#qtdlitros = (vm * temp) / 12.0
+puts "%.3f" % [(vm * temp) / 12.0]
